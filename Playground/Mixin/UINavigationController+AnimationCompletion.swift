e57672b6da8bfcc96a0569e500cbb8b982230462
Ho Lun Wan <lun.wan@keysocapp.com>
2023-01-27 17:55:57 +0800

    Extend UINavigationController's extension of animation completion

--- Playground/Mixin/UINavigationController+AnimationCompletion.swift
+++ Playground/Mixin/UINavigationController+AnimationCompletion.swift
@@ -11,31 +11,76 @@ extension UINavigationController {
     public func pushViewController(
         _ viewController: UIViewController,
         animated: Bool,
-        completion: @escaping () -> Void)
-    {
+        completion: @escaping () -> Void
+    ) {
         pushViewController(viewController, animated: animated)
+        
+        guard animated, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
+    
+    @discardableResult
+    func popViewController(
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) -> UIViewController? {
+        let vc: UIViewController? = popViewController(animated: animated)
+        
+        guard animated, vc != nil, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return vc
+        }
+        
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+        return vc
+    }
+    
+    @discardableResult
+    func popToViewController(
+        _ viewController: UIViewController,
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) -> [UIViewController]? {
+        let vcs: [UIViewController]? = popToViewController(viewController, animated: animated)
+
+        guard animated, vcs != nil, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return vcs
+        }
+        
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+        return vcs
+    }
+    
+    @discardableResult
+    func popToRootViewController(animated: Bool, completion: @escaping () -> Void) -> [UIViewController]? {
+        let vcs: [UIViewController]? = popToRootViewController(animated: animated)
+
+        guard animated, vcs != nil, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return vcs
+        }
+        
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+        return vcs
+    }
+    
+    func setViewControllers(
+        _ viewControllers: [UIViewController],
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) {
+        setViewControllers(viewControllers, animated: animated)
 
         guard animated, let coordinator = transitionCoordinator else {
             DispatchQueue.main.async { completion() }
             return
         }
-
+        
         coordinator.animate(alongsideTransition: nil) { _ in completion() }
-    }
-
-    @discardableResult
-    func popViewController(
-        animated: Bool,
-        completion: @escaping () -> Void) -> UIViewController?
-    {
-        let vc: UIViewController? = popViewController(animated: animated)
-
-        guard animated, vc != nil, let coordinator = transitionCoordinator else {
-            DispatchQueue.main.async { completion() }
-            return vc
-        }
-
-        coordinator.animate(alongsideTransition: nil) { _ in completion() }
-        return vc
     }
 }
--- Playground/NavAnimationCompletionViewController.swift
+++ Playground/NavAnimationCompletionViewController.swift
@@ -12,6 +12,9 @@ import Then
 class NavAnimationCompletionViewController: UIViewController {
     @IBOutlet var pushVCButton: UIButton!
     @IBOutlet var popVCButton: UIButton!
+    @IBOutlet var popToRootButton: UIButton!
+    @IBOutlet var set5VCsButton: UIButton!
+    @IBOutlet var popTo2ndVCButton: UIButton!
     @IBOutlet var statusLabel: UILabel!
     private var innerNav: UINavigationController?
     
@@ -20,6 +23,7 @@ class NavAnimationCompletionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         statusLabel.text = " "
+        enableButtons()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -39,13 +43,19 @@ class NavAnimationCompletionViewController: UIViewController {
     private func enableButtons() {
         pushVCButton.isEnabled = true
         popVCButton.isEnabled = true
+        popToRootButton.isEnabled = true
+        set5VCsButton.isEnabled = true
+        popTo2ndVCButton.isEnabled = (innerNav?.viewControllers.count ?? 0) > 1
     }
     
     private func disableButtons() {
         pushVCButton.isEnabled = false
         popVCButton.isEnabled = false
+        popToRootButton.isEnabled = false
+        set5VCsButton.isEnabled = false
+        popTo2ndVCButton.isEnabled = false
     }
-
+    
     
     //
     
@@ -65,5 +75,45 @@ class NavAnimationCompletionViewController: UIViewController {
             self?.statusLabel.text = "Pop completed"
             self?.enableButtons()
         })
+    }
+    
+    @IBAction func onTapPopToRootButton(_ sender: UIButton) {
+        disableButtons()
+        statusLabel.text = "Popping..."
+        innerNav?.popToRootViewController(animated: true, completion: { [weak self] in
+            self?.statusLabel.text = "Pop completed"
+            self?.enableButtons()
+        })
+    }
+    
+    @IBAction func onTapSet5VCsButton(_ sender: UIButton) {
+        disableButtons()
+        statusLabel.text = "Setting 5 view controllers..."
+
+        let vcs: [UIViewController] = [
+            createNavChildVC().then { $0.view.backgroundColor = .systemRed },
+            createNavChildVC(),
+            createNavChildVC(),
+            createNavChildVC(),
+            createNavChildVC(),
+        ]
+        innerNav?.setViewControllers(vcs, animated: true, completion: { [weak self] in
+            self?.statusLabel.text = "Set completed"
+            self?.enableButtons()
+        })
+    }
+    
+    @IBAction func onTapPopTo2ndVCButton(_ sender: UIButton) {
+        disableButtons()
+        statusLabel.text = "Popping..."
+        
+        let vcs: [UIViewController] = innerNav?.viewControllers ?? []
+        if vcs.count > 1 {
+            let vc = vcs[1]
+            innerNav?.popToViewController(vc, animated: true, completion: { [weak self] in
+                self?.statusLabel.text = "Pop completed"
+                self?.enableButtons()
+            })
+        }
     }
 }
