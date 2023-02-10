62cfd3568847ad0bd1f20da1c5d5752df27a9559
Ho Lun Wan <lun.wan@keysocapp.com>
2023-02-10 11:58:11 +0800

    Add `TagListView` draft

---
+++ Playground/DummyViewController.swift
@@ -0,0 +1,243 @@
+//
+//  DummyViewController.swift
+//  Playground
+//
+//  Created by Ho Lun Wan on 9/2/2023.
+//
+
+import UIKit
+import Then
+import TinyConstraints
+
+class DummyViewController: UIViewController {
+    @IBOutlet var scrollContentView: UIView!
+    private let tagListView = TagListView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollContentView.addSubview(tagListView)
+        
+        tagListView.do {
+            $0.edgesToSuperview()
+            $0.backgroundColor = .systemRed.withAlphaComponent(0.1)
+        }
+    }
+    
+    @IBAction func onTapAddLabelButton(_ sender: UIButton) {
+        let label = UILabel().then {
+            $0.text = "XXXXXX"
+        }
+        tagListView.addTagView(label)
+    }
+    
+    @IBAction func onTapAddLongLabelButton(_ sender: UIButton) {
+        let label = UILabel().then {
+            $0.text = "YYYYYYYYYYYYYYYYYY"
+        }
+        tagListView.addTagView(label)
+    }
+    
+    @IBAction func onTapAddVeryLongLabelButton(_ sender: UIButton) {
+        let label = UILabel().then {
+            $0.numberOfLines = 0
+            $0.text = "ZZZZZZZZZZZZZZZZZZ ZZZZZZZZZZZZZZZZZZ ZZZZZZZZZZZZZZZZZZ"
+        }
+        tagListView.addTagView(label)
+    }
+    
+    @IBAction func onTapAddViewWithRandomHeightButton(_ sender: UIButton) {
+        let height: Int = .random(in: 30...80)
+        let hue: CGFloat = .random(in: 0...1)
+        let view = UIView().then {
+            $0.backgroundColor = UIColor(hue: hue, saturation: 0.7, brightness: 1, alpha: 1)
+            $0.width(60)
+            $0.height(CGFloat(height))
+        }
+        tagListView.addTagView(view)
+    }
+    
+    @IBAction func onAlignmentSegmentValueChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                tagListView.alignment = .start
+            case 1:
+                tagListView.alignment = .center
+            default:
+                tagListView.alignment = .end
+        }
+    }
+    
+    @IBAction func onCrossAxisAlignmentSegmentValueChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                tagListView.crossAxisAlignment = .start
+            case 1:
+                tagListView.crossAxisAlignment = .center
+            default:
+                tagListView.crossAxisAlignment = .end
+        }
+    }
+    
+    @IBAction func onSpacingSliderValueChange(_ sender: UISlider) {
+        tagListView.spacing = CGFloat(sender.value)
+    }
+}
+
+//
+
+private class Row {
+    struct ViewAndSize {
+        let view: UIView
+        let size: CGSize
+    }
+    
+    private let maxLayoutWidth: CGFloat
+    let spacing: CGFloat
+    private var remainingLayoutWidth: CGFloat
+    private(set) var viewAndSizes: [ViewAndSize] = []
+    /// Row width
+    private(set) var width: CGFloat = 0
+    /// Row height
+    private(set) var height: CGFloat = 0
+    
+    init(maxLayoutWidth: CGFloat, spacing: CGFloat) {
+        self.maxLayoutWidth = maxLayoutWidth
+        self.spacing = spacing
+        remainingLayoutWidth = maxLayoutWidth
+    }
+    
+    @discardableResult
+    func addViewAndSize(view: UIView, size: CGSize) -> Bool {
+        if remainingLayoutWidth < size.width { return false }
+        
+        // Update width
+        width += size.width
+        if !viewAndSizes.isEmpty {
+            width += spacing
+        }
+        // Update height
+        height = max(height, size.height)
+        viewAndSizes.append(ViewAndSize(view: view, size: size))
+        // Minus spacing as new view always has spacing before it
+        remainingLayoutWidth = maxLayoutWidth - width - spacing
+        return true
+    }
+}
+
+class TagListView: UIView {
+    enum Alignment: CGFloat {
+        case start = 0, center = 0.5, end = 1
+    }
+    
+    private typealias ViewAndSize = (view: UIView, size: CGSize)
+    
+    private var preferredMaxLayoutWidth: CGFloat = 0
+    private var tagViews: [UIView] = []
+    private var frameForTagView: [UIView: CGRect] = [:]
+    private(set) var numberOfRows: Int = 0
+    var spacing: CGFloat = 0 {
+        didSet {
+            invalidateIntrinsicContentSize()
+            setNeedsLayout()
+        }
+    }
+    var alignment: Alignment = .start {
+        didSet {
+            invalidateIntrinsicContentSize()
+            setNeedsLayout()
+        }
+    }
+    var crossAxisAlignment: Alignment = .start {
+        didSet {
+            invalidateIntrinsicContentSize()
+            setNeedsLayout()
+        }
+    }
+
+    override class var requiresConstraintBasedLayout: Bool {
+        true
+    }
+    
+    override func layoutSubviews() {
+        if preferredMaxLayoutWidth != frame.width {
+            preferredMaxLayoutWidth = frame.width
+            invalidateIntrinsicContentSize()
+            setNeedsDisplay()
+        }
+
+        super.layoutSubviews()
+        
+        for (view, frame) in frameForTagView {
+            view.frame = frame
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        numberOfRows = 0
+        frameForTagView = [:]
+        if preferredMaxLayoutWidth == 0 || tagViews.isEmpty {
+            return super.intrinsicContentSize
+        }
+
+        numberOfRows = 1
+        var row = Row(maxLayoutWidth: preferredMaxLayoutWidth, spacing: spacing)
+        var height: CGFloat = 0
+        let layoutViewsInRow: (Row) -> Void = { row in
+            var xOffset: CGFloat = (self.preferredMaxLayoutWidth - row.width) * self.alignment.rawValue
+            for viewAndSize in row.viewAndSizes {
+                let yOffset: CGFloat = (row.height - viewAndSize.size.height) * self.crossAxisAlignment.rawValue
+                self.frameForTagView[viewAndSize.view] = CGRect(origin: CGPoint(x: xOffset, y: height + yOffset), size: viewAndSize.size)
+                xOffset += (viewAndSize.size.width + row.spacing)
+            }
+            self.numberOfRows += 1
+            height += row.height
+        }
+        for view in tagViews {
+            let viewSize = view.systemLayoutSizeFitting(CGSize(width: preferredMaxLayoutWidth, height: .infinity), withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .fittingSizeLevel)
+            
+            if !row.addViewAndSize(view: view, size: viewSize) {
+                if !row.viewAndSizes.isEmpty {
+                    // Layout views in completed row
+                    layoutViewsInRow(row)
+                    // Create a new row
+                    row = Row(maxLayoutWidth: preferredMaxLayoutWidth, spacing: spacing)
+                }
+                
+                if preferredMaxLayoutWidth < viewSize.width {
+                    // Special handling for wide view
+                    view.frame = CGRect(
+                        origin: CGPoint(x: 0, y: height),
+                        size: CGSize(width: preferredMaxLayoutWidth, height: viewSize.height))
+                    numberOfRows += 1
+                    height += viewSize.height
+                } else {
+                    // Add the view in new row
+                    row.addViewAndSize(view: view, size: viewSize)
+                }
+            }
+        }
+        
+        if !row.viewAndSizes.isEmpty {
+            layoutViewsInRow(row)
+        }
+
+        return CGSize(width: preferredMaxLayoutWidth, height: height)
+    }
+
+    //
+    
+    func addTagView(_ view: UIView) {
+        tagViews.append(view)
+        addSubview(view)
+        invalidateIntrinsicContentSize()
+        setNeedsLayout()
+    }
+    
+    func removeTagView(_ view: UIView) {
+        tagViews.removeAll(where: { $0 == view })
+        view.removeFromSuperview()
+        invalidateIntrinsicContentSize()
+        setNeedsLayout()
+    }
+}
--- README.md
+++ README.md
@@ -6,5 +6,3 @@ A list of findings/obstacles I have met
 A label breaks in line when there is not enough space to fit its content. The line breaking can be finely controlled with the use of non-breaking space.
 
 When a line breaks, the system will find normal spaces to break first, then non-breaking spaces.
-
-![the_use_of_non-breaking_space](https://user-images.githubusercontent.com/1002964/217590496-7e5d9e1f-5815-402e-bace-9a8d890d9b02.gif)
