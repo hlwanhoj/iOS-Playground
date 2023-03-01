f1a1575bcc0e68eb6ca6209f96cd02cee167dc7e
Ho Lun Wan <lun.wan@keysocapp.com>
2023-03-01 18:57:01 +0800

    Update project to support landscape mode

--- Playground/NonBreakingSpaceViewController.swift
+++ Playground/NonBreakingSpaceViewController.swift
@@ -10,8 +10,9 @@ import TinyConstraints
 import Then
 
 class NonBreakingSpaceViewController: UIViewController {
-    @IBOutlet var lbl1: UILabel!
-    @IBOutlet var lbl1TrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl1TrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,17 @@ class NonBreakingSpaceViewController: UIViewController {
         
         lbl1.attributedText = createAttrString()
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        let val = slider.value
+        coordinator.animate(alongsideTransition: { _ in
+            self.updateLbl1TrailingConstraint(sliderValue: val, viewWidth: size.width)
+        })
+    }
+    
+    //
 
     private func createAttrString() -> NSAttributedString {
         let font: UIFont = UIFont(name: "Avenir Next Regular", size: 12)!
@@ -39,7 +51,11 @@ class NonBreakingSpaceViewController: UIViewController {
         return attrStr
     }
     
+    private func updateLbl1TrailingConstraint(sliderValue: Float, viewWidth: CGFloat) {
+        lbl1TrailingConstraint.constant = (1 - CGFloat(sliderValue)) * viewWidth * 3 / 4
+    }
+    
     @IBAction func onLbl1TrailingSliderValueChanged(_ sender: UISlider) {
-        lbl1TrailingConstraint.constant = (1 - CGFloat(sender.value)) * view.frame.width * 3 / 4
+        updateLbl1TrailingConstraint(sliderValue: sender.value, viewWidth: view.frame.width)
     }
 }
